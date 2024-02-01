c28f73ecdaa246dcbc117ab141087a2e64025763
Michael Schlauch <mike@drivendata.org>
2024-02-01 08:26:08 -0500

    wait for data container to mount (can remove once entrypoint updated)

--- example_src/main.sh
+++ example_src/main.sh
@@ -3,5 +3,7 @@
 DATA_DIR=/code_execution/data
 SUBMISSION_PATH=/code_execution/submission/submission.csv
 
+sleep 15
+
 # call our script (main.py in this case) and tell it where the data and submission live
 python main.py $DATA_DIR $SUBMISSION_PATH
